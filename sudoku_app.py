bff2d938539ce665119504d8b4e13c5ca0b1876f
RockTop111469 <tats111469@gmail.com>
2026-04-11 20:57:10 +0900

    Add files via upload

--- sudoku_app.py
+++ sudoku_app.py
@@ -33,56 +33,68 @@ def is_valid(board, num, pos):
 def solve_sudoku(board):
     empty = find_empty(board)
     if not empty:
-        return True  # 解けた
+        return True
     r, c = empty
 
     for num in range(1, 10):
         if is_valid(board, num, (r, c)):
             board[r][c] = num
-
             if solve_sudoku(board):
                 return True
-
             board[r][c] = 0
 
     return False
 
 
 # -------------------------
-# 9×9 入力 UI
+# 9×9 入力 UI（罫線つき・色つき・入力チェック）
 # -------------------------
 def input_board():
     st.write("### 数独の盤面を入力してください（空欄は0）")
 
     board = np.zeros((9, 9), dtype=int)
 
-    cols = st.columns(9)
-    for c in range(9):
-        for r in range(9):
+    # CSS（罫線・色・中央寄せ）
+    st.markdown("""
+        <style>
+        .sudoku-table {
+            border-collapse: collapse;
+            margin: auto;
+        }
+        .sudoku-table td {
+            border: 1px solid #999;
+            width: 45px;
+            height: 45px;
+            text-align: center;
+            background-color: #f8f8f8;
+        }
+        .sudoku-table td:nth-child(3n) {
+            border-right: 3px solid black;
+        }
+        .sudoku-table tr:nth-child(3n) td {
+            border-bottom: 3px solid black;
+        }
+        .sudoku-table td:first-child {
+            border-left: 3px solid black;
+        }
+        .sudoku-table tr:first-child td {
+            border-top: 3px solid black;
+        }
+        </style>
+    """, unsafe_allow_html=True)
+
+    # HTML テーブル生成
+    table_html = "<table class='sudoku-table'>"
+
+    for r in range(9):
+        table_html += "<tr>"
+        for c in range(9):
             key = f"cell_{r}_{c}"
-            value = cols[c].text_input("", value="", max_chars=1, key=key)
-            if value.isdigit():
-                board[r][c] = int(value)
+            table_html += f"<td>{st.text_input('', key=key, max_chars=1)}</td>"
+        table_html += "</tr>"
 
-    return board
+    table_html += "</table>"
 
+    st.markdown(table_html, unsafe_allow_html=True)
 
-# -------------------------
-# メイン処理
-# -------------------------
-def main():
-    st.title("🧩 Sudoku Solver（手入力版）")
-
-    board = input_board()
-
-    if st.button("Solve"):
-        board_copy = board.copy()
-        if solve_sudoku(board_copy):
-            st.write("### ✔ 解答:")
-            st.write(board_copy)
-        else:
-            st.error("解けませんでした。")
-
-
-if __name__ == "__main__":
-    main()
+    # 入力
