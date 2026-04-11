f691d91be4e60b507a63733b313e74db1da843b8
RockTop111469 <tats111469@gmail.com>
2026-04-11 22:05:42 +0900

    Add files via upload

--- sudoku_app.py
+++ sudoku_app.py
@@ -1,62 +1,27 @@
-import streamlit as st
-import numpy as np
-
-# -------------------------
-# 数独ソルバー
-# -------------------------
-def find_empty(board):
-    for r in range(9):
-        for c in range(9):
-            if board[r][c] == 0:
-                return r, c
-    return None
-
-def is_valid(board, num, pos):
-    r, c = pos
-    if num in board[r]: return False
-    if num in board[:, c]: return False
-    br, bc = (r//3)*3, (c//3)*3
-    if num in board[br:br+3, bc:bc+3]: return False
-    return True
-
-def solve_sudoku(board):
-    empty = find_empty(board)
-    if not empty:
-        return True
-    r, c = empty
-    for num in range(1, 10):
-        if is_valid(board, num, (r, c)):
-            board[r][c] = num
-            if solve_sudoku(board):
-                return True
-            board[r][c] = 0
-    return False
-
-
-# -------------------------
-# 入力UI（セル完全囲み罫線）
-# -------------------------
 def input_board():
     st.write("### 数独の盤面を入力してください（空欄は0）")
 
     board = np.zeros((9, 9), dtype=int)
 
-    # CSS（入力欄の中央寄せ・サイズ）
+    # CSS（セルを完全に囲む）
     st.markdown("""
         <style>
-        .sudoku-input input {
+        .cell-wrapper {
+            position: relative;
+            width: 45px;
+            height: 45px;
+        }
+        .cell-input input {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 45px !important;
+            height: 45px !important;
             text-align: center;
             font-size: 22px;
-            height: 45px;
-            width: 45px;
             padding: 0;
-        }
-        .cell-box {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 45px;
-            width: 45px;
+            border: none;
+            background: transparent;
         }
         </style>
     """, unsafe_allow_html=True)
@@ -65,33 +30,31 @@ def input_board():
         cols = st.columns(9, gap="small")
         for c in range(9):
 
-            # 枠線の太さを決める
+            # 枠線の太さ
             style = ""
-
-            # 上
             style += "border-top: {} solid black;".format("3px" if r % 3 == 0 else "1px")
-            # 左
             style += "border-left: {} solid black;".format("3px" if c % 3 == 0 else "1px")
-            # 下
             style += "border-bottom: {} solid black;".format("3px" if r == 8 else "1px")
-            # 右
             style += "border-right: {} solid black;".format("3px" if c == 8 else "1px")
 
             key = f"cell_{r}_{c}"
 
             with cols[c]:
+                # 枠線つきの箱
                 st.markdown(
-                    f"<div class='cell-box' style='{style}'>",
+                    f"<div class='cell-wrapper' style='{style}'>",
                     unsafe_allow_html=True
                 )
 
+                # 中に text_input を絶対配置
                 v = st.text_input(
                     "",
                     key=key,
                     max_chars=1,
                     label_visibility="collapsed",
                     placeholder=" ",
-                    help="1〜9の数字を入力"
+                    help="1〜9の数字を入力",
+                    key=f"cell_{r}_{c}"
                 )
 
                 st.markdown("</div>", unsafe_allow_html=True)
@@ -103,47 +66,3 @@ def input_board():
                 board[r][c] = 0
 
     return board
-
-
-# -------------------------
-# 解答表示（同じ罫線で表示）
-# -------------------------
-def show_solution(board):
-    st.write("### ✔ 解答:")
-
-    for r in range(9):
-        cols = st.columns(9, gap="small")
-        for c in range(9):
-
-            style = ""
-            style += "border-top: {} solid black;".format("3px" if r % 3 == 0 else "1px")
-            style += "border-left: {} solid black;".format("3px" if c % 3 == 0 else "1px")
-            style += "border-bottom: {} solid black;".format("3px" if r == 8 else "1px")
-            style += "border-right: {} solid black;".format("3px" if c == 8 else "1px")
-
-            with cols[c]:
-                st.markdown(
-                    f"<div style='{style}; text-align:center; font-size:22px; padding-top:8px;'>"
-                    f"<b>{board[r][c]}</b></div>",
-                    unsafe_allow_html=True
-                )
-
-
-# -------------------------
-# メイン
-# -------------------------
-def main():
-    st.title("🧩 Sudoku Solver（セル完全囲み罫線版）")
-
-    board = input_board()
-
-    if st.button("Solve"):
-        board_copy = board.copy()
-        if solve_sudoku(board_copy):
-            show_solution(board_copy)
-        else:
-            st.error("解けませんでした。")
-
-
-if __name__ == "__main__":
-    main()
